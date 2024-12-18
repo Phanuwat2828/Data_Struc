b1a1347741ff61c48e96f2ef3fab3f00325c6f93
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-12-18 12:44:05 +0700

    update

---
+++ araay_sort/quick_sort.cpp
@@ -0,0 +1,9 @@
+
+void quick_sort(int data[]){
+    // 
+}
+
+int main(){
+    int number[] = {34, 10, 39, 49, 24, 42, 9,0,99};
+
+}
---
+++ araay_sort/shell_sort.cpp
@@ -0,0 +1,28 @@
+
+
+void shell_sort(int data[]){
+    // lenght/2 {lenght = count of array} and lenght have to than more [ 0 ]
+
+    
+    int count = 9/2;
+    while (count>=0)
+    {
+        
+    }
+    
+    for(int i=0;i<count;i++){
+        
+    }
+
+
+
+    
+}
+
+
+int main(){
+    int number[] = {34, 10, 39, 49, 24, 42, 9,0,99};
+
+    
+
+}
