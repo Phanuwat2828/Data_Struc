8bf4229a51278563d297f6ffa4c7582690a05653
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-12-19 12:42:33 +0700

    update

--- araay_sort/shell_sort.cpp
+++ araay_sort/shell_sort.cpp
@@ -1,28 +1,29 @@
-
-
-void shell_sort(int data[]){
-    // lenght/2 {lenght = count of array} and lenght have to than more [ 0 ]
-
-    
-    int count = 9/2;
-    while (count>=0)
-    {
+#include <stdio.h>
+int* shell_sort(int data[],int N){
+    int k = N/2;
+    while(k>=1){
+        int i=k;
+        while (i<=N-1){
+           int temp = data[i];
+           int j=i;
+           while(j>k-1 && data[j-k] > temp){
+                data[j] = data[j-k];
+                j = j-k ;
+                data[j] = temp;
+           }
+           i++;
+        }
+        k=k/2;
         
     }
-    
-    for(int i=0;i<count;i++){
-        
-    }
-
-
-
-    
+    return data; 
 }
-
-
 int main(){
     int number[] = {34, 10, 39, 49, 24, 42, 9,0,99};
-
+    int* data = shell_sort(number,9);
+    for(int i=0;i<9;i++){
+        printf("%d\n",data[i]);
+    }
     
 
 }
